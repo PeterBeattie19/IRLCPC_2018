bb33f202eb39d1ddc3ebd2f093fe8f4419d6547f
Peter Beattie <peterbeattie19@gmail.com>
2018-03-12 16:00:42 +0000

    Update P2.py

--- P2.py
+++ P2.py
@@ -12,14 +12,27 @@ t2 = [0]*n
 
 t1[0] = 1
 
-for i in range(n):
+for i in range(1,n):
+  found = False
   for j in range(n):
     if arr[i][j] == 1:
       if t1[j]==1:
-        t2[j]=1
+        t2[i]=1
+        found = True
         break
-  t2[i] = i
+  if found == False:
+    t1[i] = 1
+  
   
 print t1
 print t2
+  
+for i in range(0,n):
+  if t1[i] == 1:
+    print i,
     
+print 
+
+for i in range(0,n):
+  if t2[i] == 1:
+    print i,
